a4b042fbde93b187187057803b30e83c1d132057
Zhenwei Zhu <garyzhu1996@gmail.com>
2023-08-23 15:04:14 +0800

    Update logging.py

--- utils/logging.py
+++ utils/logging.py
@@ -15,3 +15,8 @@ def debug(str):
 def info(str):
     if torch.distributed.get_rank() == 0:
         logging.info(str)
+
+
+def warn(str):
+    if torch.distributed.get_rank() == 0:
+        logging.warn(str)
